d5276acbb788bf9aacda5598ca7a431d22cb9b67
chikaoka sanemitsu <83085478+sanepitcom@users.noreply.github.com>
2021-08-19 02:10:49 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/sanepit/checkbox/flutter_checkbox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sanepit.checkbox.flutter_checkbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  build(BuildContext context) => MaterialApp(
+    title: 'Checkbox Example',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Checkbox Example'),
+      ),
+      body:Padding(
+        padding: EdgeInsets.all(15),
+        child: CheckBoxExample(),
+      ),
+    ),
+  );
+}
+
+class CheckBoxExample extends StatefulWidget {
+  @override
+  createState() => _CheckBoxExampleState();
+}
+
+class _CheckBoxExampleState extends State<CheckBoxExample> {
+  var isChecked = false;
+  var label = 'チェックボックスの状態が表示されます。';
+
+  @override
+  build(BuildContext context) => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text('$label'),
+      SizedBox(height: 15,),
+      Row(
+        children: [
+          Checkbox(
+            value: isChecked,
+            onChanged: (value) => setState(() {
+              label = 'おかゆ' + (value! ? 'が選択されました。' : 'の選択が解除されました。');
+              setState(() => isChecked = value);
+            }),
+          ),
+          SizedBox(width: 5,),
+          Text('おかゆ'),
+        ],
+      ),
+    ]
+  );
+}
